0c75a4ccfd1708a15a52908031a8963d79ebbdb3
pirzer <pirzer1@yahoo.com>
2023-01-22 17:15:24 +0000

    adding Coverage tests

--- todo/test_models.py
+++ todo/test_models.py
@@ -7,3 +7,7 @@ class TestModels(TestCase):
     def test_done_defaults_to_false(self):
         item = Item.objects.create(name='Test Todo Item', done=False)
         self.assertFalse(item.done)
+
+    def test_item_string_method_returns_name(self):
+        item = Item.objects.create(name='Test Todo Item', done=False)
+        self.assertEqual(str(item), 'Test Todo Item')
