4a8c2afce5687ccf163e69be876f956154b30764
pirzer <pirzer1@yahoo.com>
2023-01-22 14:48:43 +0000

    adding done equal to False at line 18 and line 28

--- todo/test_views.py
+++ todo/test_views.py
@@ -14,22 +14,22 @@ class TestViews(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'todo/add_item.html')
 
-    # def test_get_edit_item_page(self):
-    #     item = Item.objects.create(name='Test Todo Item')
-    #     response = self.client.get(f'/edit/{item.id}')
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'todo/edit_item.html')
+    def test_get_edit_item_page(self):
+        item = Item.objects.create(name='Test Todo Item', done=False)
+        response = self.client.get(f'/edit/{item.id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/edit_item.html')
 
     def test_can_add_item(self):
         response = self.client.post('/add', {'name': 'Test Added Item'})
         self.assertRedirects(response, '/')
 
-    # def test_can_delete_item(self):
-    #     item = Item.objects.create(name='Test Todo Item')
-    #     response = self.client.get(f'/delete/{item.id}')
-    #     self.assertRedirects(response, '/')
-    #     existing_items = Item.objects.filter(id=item.id)
-    #     self.assertEqual(len(existing_items), 0)
+    def test_can_delete_item(self):
+        item = Item.objects.create(name='Test Todo Item', done=False)
+        response = self.client.get(f'/delete/{item.id}')
+        self.assertRedirects(response, '/')
+        existing_items = Item.objects.filter(id=item.id)
+        self.assertEqual(len(existing_items), 0)
 
     def test_can_toggle_item(self):
         item = Item.objects.create(name='Test Todo Item', done=True)
