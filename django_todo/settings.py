587631f4e3ba8bb63b078f9e0c8bead960ba4c8a
pirzer <pirzer1@yahoo.com>
2023-01-25 22:08:33 +0000

    heroku deployment and elephant data connect

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+import dj_database_url
+
+
+if os.path.isfile("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,12 +79,17 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ: 
+    DATABASES = { 
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) 
     }
-}
+else: 
+    DATABASES = { 
+        'default': { 
+            'ENGINE': 'django.db.backends.sqlite3', 
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), 
+         } 
+    }
 
 
 # Password validation
