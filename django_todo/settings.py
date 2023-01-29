d356c5125fc943d3a6314289bc356e5a64a1ed9d
pirzer <pirzer1@yahoo.com>
2023-01-29 22:04:50 +0000

    adding if development line 35 and duplica line 38 in line 36 changing the argument

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -32,7 +32,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-t-bf(me49-gtum925hlam
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+if development:
+    ALLOWED_HOSTS = ['localhost']
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+
 
 # Application definition
 
